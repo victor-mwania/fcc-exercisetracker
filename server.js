9151b89fddd4871cc745f6160f2cd44bf44d5cd2
victor-mwania <victormwnza@gmail.com>
2019-01-15 17:40:03 +0300

    user can register with a unique username

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
 const cors = require('cors')
 
+const User = require('.//models/user')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://victor:victor1234@cluster0-shard-00-00-mgep6.mongodb.net:27017,cluster0-shard-00-01-mgep6.mongodb.net:27017,cluster0-shard-00-02-mgep6.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 
 
@@ -16,11 +18,33 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+// Create a New User endpoint
+app.post('/api/exercise/new-user', function (req, res) {
 
-
+  let saveuser = new User({
+    username: req.body.username
+  })
+  //check if user exists. If not create a new user
+  User.findOne({username: req.body.username} ,(err, user)=>{if (err){
+    res.send(err)
+  }}).then((user) => {
+    if(!user ){
+      saveuser.save().then(function(result){
+        res.json(result)
+      })
+    }
+    else{
+      res.send("User already exists");
+    }
+  })
+  }
+)
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
+  return next({
+    status: 404,
+    message: 'not found'
+  })
 })
 
 // Error Handling middleware
