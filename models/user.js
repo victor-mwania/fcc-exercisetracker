3837917a4463e49acc2399c76edc46fd10042816
victor-mwania <victormwnza@gmail.com>
2019-01-15 17:57:14 +0300

    generate short id

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const shortid = require('shortid');
 
 const userSchema = mongoose.Schema({
-    username: String
+    username: String,
+    _id: {
+        'type': String,
+        'default': shortid.generate
+    }
 })
 
 const User = mongoose.model("User", userSchema)
