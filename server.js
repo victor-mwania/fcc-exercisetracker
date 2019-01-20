875ec0e9c54ca1dc25cbcf6578c53103e5c10dc6
victor-mwania <victormwnza@gmail.com>
2019-01-20 11:30:26 +0300

    Created an endpoint to add new exercises

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const User = require('.//models/user')
+const Exercise = require('.//models/exercie')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://victor:victor1234@cluster0-shard-00-00-mgep6.mongodb.net:27017,cluster0-shard-00-01-mgep6.mongodb.net:27017,cluster0-shard-00-02-mgep6.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
 app.use(cors())
@@ -25,20 +26,84 @@ app.post('/api/exercise/new-user', function (req, res) {
     username: req.body.username
   })
   //check if user exists. If not create a new user
-  User.findOne({username: req.body.username} ,(err, user)=>{if (err){
-    res.send(err)
-  }}).then((user) => {
-    if(!user ){
-      saveuser.save().then(function(result){
+  User.findOne({
+    username: req.body.username
+  }, (err, user) => {
+    if (err) {
+      res.send(err)
+    }
+  }).then((user) => {
+    if (!user) {
+      saveuser.save().then(function (result) {
         res.json(result)
       })
-    }
-    else{
+    } else {
       res.send("User already exists");
     }
   })
-  }
-)
+})
+// add excercise
+app.post('/api/exercise/add', function (req, res) {
+ 
+    let userId =req.body.userId;
+    let description =req.body.description;
+    let duration =req.body.duration;
+    let date =req.body.date;
+  User.findById({
+    _id: req.body.userId
+  }, (err, user) => {// check f date is null then use the default date (date.now)
+    if(err){
+      res.send(err)
+    }
+    if (!user) {
+      res.json({
+        error: "UserId is not found"
+      })
+    } 
+    else {
+      if(!description) {
+        res.json({description: "Please input a description"})
+      }
+      if(!duration) {
+        res.json({duration: " Please input duration of the exercise"})
+      }
+      if(!date) {
+      let  xer = new Exercise ({
+          userId,
+          description,
+          duration,
+          date : Number(Date.now()) 
+        })
+        xer.save().then((err, exercise) => {
+          if(err){
+            res.send(err)
+          }
+          else {
+            res.send(exercise)
+          }
+        })
+      }else{
+        let saveExercise = new Exercise ({
+          userId,
+          description,
+          duration,
+          date
+        })
+        saveExercise.save().then((err, exercise) => {
+          if(err){
+            res.send(err)
+          }
+          else{
+            res.send(exercise)
+          }
+        })
+      }
+     
+    }
+
+  })
+
+})
 // Not found middleware
 app.use((req, res, next) => {
   return next({
